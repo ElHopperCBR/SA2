6b44762a5a2b2a24b0a819415079380853811d45
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-03-15 20:13:44 -0300

    ultima alteração antes de iniciar o back end

--- SA2/Program.cs
+++ SA2/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var app = builder.Build();
+var app = builder.Build(); //
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
